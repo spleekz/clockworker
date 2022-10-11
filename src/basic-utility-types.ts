6525c2154f2be11ffc2f0031672644802fbdcf31
spleekz <Shutnik-12K@yandex.ru>
2022-10-11 17:32:47 +0300

    use shorter type names

--- src/basic-utility-types.ts
+++ src/basic-utility-types.ts
@@ -8,11 +8,11 @@ export type Callback = () => void
 
 export type FC<T = AnyObject> = React.FC<PropsWithChildren<T>>
 
-export type NotUndefinded<Type> = Type extends undefined ? never : Type
+export type NotUndefinded<T> = T extends undefined ? never : T
 
-export type RequiredBy<Type, Key extends keyof Type> = Omit<Type, Key> &
+export type RequiredBy<T, K extends keyof T> = Omit<T, K> &
   Required<{
-    [P in Key]: NotUndefinded<Type[Key]>
+    [P in K]: NotUndefinded<T[P]>
   }>
 
 export type NonNullableProperties<T> = { [P in keyof T]: NonNullable<T[P]> }
