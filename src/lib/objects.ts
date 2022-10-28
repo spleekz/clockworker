70040778ce209a5a0938750f8c0f877ec94c4689
spleekz <Shutnik-12K@yandex.ru>
2022-10-29 00:08:34 +0300

    add objects merge function

--- src/lib/objects.ts
+++ src/lib/objects.ts
@@ -1,3 +1,30 @@
+import { AnyObject, Merge } from 'basic-utility-types'
+
 export const isObject = (value: any): boolean => {
   return typeof value === 'object' && !Array.isArray(value) && value !== null
+}
+
+export const merge = <T1 extends AnyObject, T2 extends AnyObject>(
+  object1: T1,
+  object2: T2,
+): Merge<T1, T2> => {
+  const merged = object1
+
+  const mergeObjects = (target: AnyObject, source: AnyObject): void => {
+    Object.keys(source).forEach((key) => {
+      if (target[key] === undefined) {
+        target[key] = source[key]
+      } else {
+        if (isObject(target[key]) && isObject(source[key])) {
+          mergeObjects(target[key], source[key])
+        } else {
+          target[key] = source[key]
+        }
+      }
+    })
+  }
+
+  mergeObjects(merged, object2)
+
+  return merged as Merge<T1, T2>
 }
